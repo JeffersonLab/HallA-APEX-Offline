e18a07941a4593225d972e75ccce67346e8f7680
john w <80jtaw@gmail.com>
2019-09-13 11:33:37 +0100

    Added Calibrations folder with a README and file defining location of rootfiles. Also added a Cherenkov folder to this with a script for the left and the right arm (of the HRS) that finds the pedestals and gains for all channels (for a particular run or set of concurrent runs).

---
+++ Calibrations/file_def.h
@@ -0,0 +1,6 @@
+#include "TObject.h" //trick to avoid error: `Int_t' does not name a type
+
+
+
+// Directory where root files are stored (run number replaced with %d)
+TString ROOTFILE_DIR = "/w/work3/home/johnw/Rootfiles/apex_%d_10_9_2019.root";
