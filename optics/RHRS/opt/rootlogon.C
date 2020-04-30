821163671c430e8bbb99deb41b76a24a3187c778
Sean Jeffas <sean.jeffas@stonybrook.edu>
2020-04-30 14:10:04 -0400

    fixed small errors

--- optics/RHRS/opt/rootlogon.C
+++ optics/RHRS/opt/rootlogon.C
@@ -4,12 +4,10 @@
     gInterpreter->AddIncludePath("$ANALYZER/src/");
     gInterpreter->AddIncludePath("$ANALYZER/hana_decode");
 
-    gROOT->LoadMacro("cut_R.C");
     gROOT->LoadMacro("ROpticsOpt.C+");
     gROOT->LoadMacro("ROpticsOptScript.C");
+    gROOT->LoadMacro("cut_R.C");
 
-    //gROOT->LoadMacro("2matrix/ROpticsOpt.C+");
-    //gROOT->LoadMacro("2matrix/ROpticsOptScript.C");
     
     gStyle->SetPalette(1);
 }
